6e9f8eba188f5dd8abd99ee7bc765928d0408ec0
William Derek Dias <williamdkdevops@gmail.com>
2021-06-26 20:18:26 -0300

    REST API that manages the access control to employee data of a company by validating their access level and professional category

---
+++ src/main/java/br/com/supernova/accesscontrol/AccessControlApplication.java
@@ -0,0 +1,13 @@
+package br.com.supernova.accesscontrol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccessControlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccessControlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/supernova/accesscontrol/AccessControlApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.supernova.accesscontrol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccessControlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
